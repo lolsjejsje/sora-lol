5fc1cf221961c12afe2454acc24d5b89c63e5b42
lolsjejsje <lolsjejsje@gmail.com>
2025-04-22 04:28:43 -0700

    Update watchhentai.js

--- watchhentai.js
+++ watchhentai.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // Search Results: target the .result-item containers
   searchResults: ($) => $('div.result-item article').map((i, el) => {
     const a = $(el).find('div.thumbnail.animation-2 a');
     return {
@@ -9,19 +8,16 @@ module.exports = {
     };
   }).get(),
 
-  // Details extraction (series detail page)
   extractDetails: ($) => ({
     description: $('div.contenido p').first().text().trim() || '',
     aliases:     null,
     airdate:     $('div.meta span.year').first().text().trim() || null
   }),
 
-  // Episodes extraction (if series lists episodes)
   extractEpisodes: ($) => $('ul.episodes-list li a').map((i, el) => ({
     href:   $(el).attr('href'),
-    number: $(el).text().trim().match(/\\d+/)[0]
+    number: $(el).text().trim().match(/\d+/)[0]
   })).get(),
 
-  // Stream URL (embedded iframe)
   extractStreamUrl: ($) => $('iframe').attr('src') || ''
 };
