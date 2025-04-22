cafb372fe5042ec23d4bb49cdd8c666722cec4a9
lolsjejsje <lolsjejsje@gmail.com>
2025-04-22 04:30:58 -0700

    Update watchhentai.js

--- watchhentai.js
+++ watchhentai.js
@@ -1,23 +1,29 @@
 module.exports = {
-  searchResults: ($) => $('div.result-item article').map((i, el) => {
-    const a = $(el).find('div.thumbnail.animation-2 a');
+  // Search Results
+  searchResults: ($) => $('.result-item').map((i, el) => {
+    const $el = $(el);
+    const link = $el.find('div.image a');
     return {
-      title: a.find('img').attr('alt').trim(),
-      href:  a.attr('href'),
-      image: a.find('img').attr('src')
+      title: $el.find('div.details .title a').text().trim() ||
+             link.find('img').attr('alt').trim(),
+      href:  link.attr('href'),
+      image: link.find('img').attr('src')
     };
   }).get(),
 
+  // Details
   extractDetails: ($) => ({
     description: $('div.contenido p').first().text().trim() || '',
     aliases:     null,
     airdate:     $('div.meta span.year').first().text().trim() || null
   }),
 
+  // Episodes
   extractEpisodes: ($) => $('ul.episodes-list li a').map((i, el) => ({
     href:   $(el).attr('href'),
     number: $(el).text().trim().match(/\d+/)[0]
   })).get(),
 
+  // Stream URL
   extractStreamUrl: ($) => $('iframe').attr('src') || ''
 };
