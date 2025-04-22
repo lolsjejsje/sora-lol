cfd3b478f4cc8b1b8630e7c86df6ade805a8b679
lolsjejsje <lolsjejsje@gmail.com>
2025-04-22 04:26:07 -0700

    Update watchhentai.js

--- watchhentai.js
+++ watchhentai.js
@@ -1,23 +1,27 @@
 module.exports = {
-  searchResults: ($) => $('div.thumbnail').map((i, el) => {
-    const a = $(el).find('a');
+  // Search Results: target the .result-item containers
+  searchResults: ($) => $('div.result-item article').map((i, el) => {
+    const a = $(el).find('div.thumbnail.animation-2 a');
     return {
-      title:  a.find('img').attr('alt').trim(),
-      href:   a.attr('href'),
-      image:  a.find('img').attr('src')
+      title: a.find('img').attr('alt').trim(),
+      href:  a.attr('href'),
+      image: a.find('img').attr('src')
     };
   }).get(),
 
+  // Details extraction (series detail page)
   extractDetails: ($) => ({
-    description: $('div#description, div.entry-content p').first().text().trim() || '',
+    description: $('div.contenido p').first().text().trim() || '',
     aliases:     null,
-    airdate:     $('li:contains("First air date")').text().replace('First air date', '').trim() || null
+    airdate:     $('div.meta span.year').first().text().trim() || null
   }),
 
+  // Episodes extraction (if series lists episodes)
   extractEpisodes: ($) => $('ul.episodes-list li a').map((i, el) => ({
     href:   $(el).attr('href'),
-    number: $(el).text().trim().match(/\d+/)[0]
+    number: $(el).text().trim().match(/\\d+/)[0]
   })).get(),
 
+  // Stream URL (embedded iframe)
   extractStreamUrl: ($) => $('iframe').attr('src') || ''
 };
